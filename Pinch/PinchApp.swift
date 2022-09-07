d6404f9402f02fe5d16ecd7be22583abcdb2a305
Martin Stofko <martinstofko114@gmail.com>
2022-09-07 13:34:26 -0700

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Martin Stofko on 9/7/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

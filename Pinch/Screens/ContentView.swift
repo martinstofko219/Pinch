5ecc537978448aaf05d2a61d3316f79748e24205
Martin Stofko <martinstofko114@gmail.com>
2022-09-09 09:10:00 -0700

    Added drag gesture support to the app and cleaned up zooming features.

--- Pinch/Screens/ContentView.swift
+++ Pinch/Screens/ContentView.swift
@@ -10,6 +10,24 @@ import SwiftUI
 struct ContentView: View {
     @State private var isAnimating = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
+    
+    private func imageDoubleTap() {
+        if imageScale == 1 {
+            withAnimation(.spring()) {
+                imageScale = 5
+            }
+        } else {
+            resetImageState()
+        }
+    }
+    
+    private func resetImageState() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
     
     var body: some View {
         NavigationView {
@@ -21,19 +39,23 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(imageOffset)
                     .animation(.linear(duration: 1), value: isAnimating)
                     .scaleEffect(imageScale)
-                    .onTapGesture(count: 2) {
-                        if imageScale == 1 {
-                            withAnimation(.spring()) {
-                                imageScale = 5
-                            }
-                        } else {
-                            withAnimation(.spring()) {
-                                imageScale = 1
-                            }
-                        }
-                    }
+                    .onTapGesture(count: 2) { imageDoubleTap() }
+                    .gesture(
+                        DragGesture()
+                            .onChanged({ value in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = value.translation
+                                }
+                            })
+                            .onEnded({ value in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            })
+                    )
             }
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
